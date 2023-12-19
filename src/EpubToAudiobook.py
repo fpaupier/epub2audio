eaca2abe2a2b3815ed697afda17838f92fd3e99d
François Paupier <francois.paupier@gmail.com>
2023-12-19 09:19:12 +0100

    sync with https://github.com/fpaupier/epub2audio/pull/1/commits/670939c4991cec34d59d62c0f849133ba5a1df7d

--- src/EpubToAudiobook.py
+++ src/EpubToAudiobook.py
@@ -148,6 +148,8 @@ class EpubToAudiobook:
             .replace(":", ", ")
             .replace("''", ", ")
             .replace("’", "'")
+            .replace(" . . . ", ", ")
+            .replace("... ", ", ")
             .replace("\n", " \n")
             .strip()
         )
@@ -169,7 +171,7 @@ class EpubToAudiobook:
                 continue
             print("Length: " + str(len(text)))
             print("Part: " + str(len(self.chapters_to_read) + 1))
-            print(text[:256])
+            print(text[:1024])
             self.chapters_to_read.append(text)
         print("Number of chapters to read: " + str(len(self.chapters_to_read)))
         if self.end == 999:
